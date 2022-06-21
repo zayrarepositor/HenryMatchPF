61f08691717d650c10add6ab7f04d940891ad339
Giuxpro <albertogiusepp@gmail.com>
2022-06-21 14:37:44 -0300

    cors added

--- api/src/app.js
+++ api/src/app.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
-const server = express()
+const server = express();
+const cors = require("cors")
+
 
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
