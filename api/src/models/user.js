34a621ef37e4e3faf5686aca143a9032511366e6
JuanjoChiliutti <juanjo.chiliutti@gmail.com>
2022-06-21 15:22:54 -0300

    models created

---
+++ api/src/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require ('mongoose')
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        // consultar cual es el fin de esto.
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+    },
+    genderInt: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    henryLevel: {
+        type: String,
+        enum: ['m1', 'm2', 'm3', 'm4', 'graduate']
+    }
+})
+
+module.exports = mongoose.model('Users', userSchema)
