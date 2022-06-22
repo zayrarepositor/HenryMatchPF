8926f9f1e7953d39639939a96e1f0d01808cc82d
Giuxpro <albertogiusepp@gmail.com>
2022-06-22 18:03:37 -0300

    change form client and api

--- api/src/app.js
+++ api/src/app.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV !== 'production'){
+ require('dotenv').config(); 
+ }
 const express = require("express")
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -7,6 +10,7 @@ const routes = require('./routes/index.js');
 const server = express();
 const cors = require("cors")
 
+server.set('port', process.env.PORT || 9000)
 
 server.name = 'API';
 
@@ -16,12 +20,14 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:9000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*');//'http://localhost:9000' // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+
+
 
 server.use('/', routes);
 
