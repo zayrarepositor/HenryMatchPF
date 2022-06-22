1e115e285ecd483d75e57bcf4b6296e9e0a20bf5
Giuxpro <albertogiusepp@gmail.com>
2022-06-22 01:38:38 -0300

    feature route put created

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { userGet } = require('../controllers/userGet');
 const { userPost } = require('../controllers/userPost');
+const { userPut } = require('../controllers/userPut');
 
 const router = express.Router();
 
 router.get("/users",userGet);
 router.post('/users', userPost);
+router.put("/users/:id", userPut)
 
 
 
