4e11d5367de3eaac8dc542a85c0fe3b5106667d0
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-06-22 16:17:28 -0300

    WelcomeLogin

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import OnBoarding from "./pages/OnBoarding";
-import "./index.css";
-import Dashboard from "./pages/Dashboard";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/onboarding" element={<OnBoarding />} />
-      </Routes>
-    </BrowserRouter>
+ <App />
   </React.StrictMode>
-);
+)
+
