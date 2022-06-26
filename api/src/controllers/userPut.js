33b3acd2a17c2272d378debde8a506a7342826c7
JuanjoChiliutti <juanjo.chiliutti@gmail.com>
2022-06-26 11:37:28 -0300

    user models updated

--- api/src/controllers/userPut.js
+++ api/src/controllers/userPut.js
@@ -1,19 +1,43 @@
 const userSchema = require("../models/user");
 
-
-const userPut = (req, res) =>{
-    const {id} = req.params;
-<<<<<<< HEAD
-    const {name, age, birthday, nickname, email, password, genderInt, gender, image, description, henryLevel} = req.body
-=======
-    const {name, age ,birthday, nickname, email, password, image, description, henryLevel} = req.body
->>>>>>> cc154f79e04170b3ac582cef928884acd6eb25f0
-    userSchema
-        .updateOne({_id:id},{ $set: {name, age, birthday, nickname, email, password, image, genderInt, gender, description, henryLevel}})
-        .then(data => res.json(data))
-        .catch((error) => res.json({message:error}));
-}
+const userPut = (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    age,
+    birthday,
+    nickname,
+    email,
+    password,
+    image,
+    genderInt,
+    gender,
+    description,
+    henryLevel,
+  } = req.body;
+  userSchema
+    .updateOne(
+      { _id: id },
+      {
+        $set: {
+          name,
+          age,
+          birthday,
+          nickname,
+          email,
+          password,
+          image,
+          genderInt,
+          gender,
+          description,
+          henryLevel,
+        },
+      }
+    )
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+};
 
 module.exports = {
-    userPut
+  userPut,
 };
