b9de922445cdc4bdddc1f41ef91514bcc061786f
Martina Ortiz Novero <martuortiznov@gmail.com>
2022-06-26 23:42:29 -0300

    haciendo cambios

--- api/src/controllers/userPut.js
+++ api/src/controllers/userPut.js
@@ -1,15 +1,30 @@
+
+
+/* const Users = require('../models/user')
+const userPut = async (req, res) => {
+    const { name, age, birthday, nickname, email, password, genderInt, gender, image, description, henryLevel } = req.body
+    try {
+        const updatedUser = await Users.findByIdAndUpdate(id, {
+            name, age, birthday, nickname, email, password, genderInt, gender, image, description, henryLevel
+        })
+        console.log(updatedUser,'UPDATEUSER')
+        res.send(updatedUser)
+    } catch (error) {
+        console.log(error.message)
+        return error.message
+    }
+} 
+ */
+
 const userSchema = require("../models/user");
 
 
 const userPut = (req, res) =>{
     const {id} = req.params;
-<<<<<<< HEAD
-    const {name, age, birthday, nickname, email, password, genderInt, gender, image, description, henryLevel} = req.body
-=======
-    const {name, age ,birthday, nickname, email, password, image, description, henryLevel} = req.body
->>>>>>> cc154f79e04170b3ac582cef928884acd6eb25f0
+    const {name, age, birthday, nickname,premium,active, email, image, genderInt, gender, description, henryLevel, likeRecieved, likeGiven, matches} = req.body
+
     userSchema
-        .updateOne({_id:id},{ $set: {name, age, birthday, nickname, email, password, image, genderInt, gender, description, henryLevel}})
+        .updateOne({_id:id},{ $set: {name, age, birthday, nickname,premium,active, email, image, genderInt, gender, description, henryLevel, likeRecieved, likeGiven, matches}})
         .then(data => res.json(data))
         .catch((error) => res.json({message:error}));
 }
@@ -17,3 +32,16 @@ const userPut = (req, res) =>{
 module.exports = {
     userPut
 };
+
+
+/* const userPut = (req, res) =>{
+    const {id} = req.params;
+
+    const {name, age, birthday, nickname, email, password, genderInt, gender, image, description, henryLevel} = req.body
+
+    userSchema
+        .updateOne({_id:id},{ $set: {name, age, birthday, nickname, email, password, image, genderInt, gender, description, henryLevel}})
+        .then(data => res.json(data))
+        .catch((error) => res.json({message:error}));
+} 
+ */
