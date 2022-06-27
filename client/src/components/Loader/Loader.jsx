6ae3259e6a9e9fc118836e0738e87154483477a4
zayrarepositor <zayra.contacto@gmail.com>
2022-06-27 10:15:32 -0300

    loader(loader): add a backdrop
    loader ready

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,18 +1,13 @@
 //======ESTILO E IMAGENES
-import { Stack, CircularProgress } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
-
-//ELEGIR COLOR DEL LOADER
-{
-  /* <CircularProgress color="success"||"secondary"||></CircularProgress> */
-}
 
 const Loader = () => {
   return (
     <Backdrop
       sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={open}>
-      <CircularProgress color="primary"></CircularProgress>
+      <CircularProgress></CircularProgress>
     </Backdrop>
   );
 };
