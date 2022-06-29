ad2be0ec0c41b592a26682db6fb47f6b6fa3f5b0
Arturo <arturofsx@gmail.com>
2022-06-29 16:54:06 -0300

    act

--- api/src/controllers/userPut.js
+++ api/src/controllers/userPut.js
@@ -3,14 +3,15 @@ const userSchema = require("../models/user");
 
 const userPut = (req, res) =>{
     const {id} = req.params;
-    const {name, age, birthday, nickname,premium,active, email, image, genderInt, gender, description, henryLevel, likeRecieved, likeGiven, matches} = req.body
+    const {name, age, birthday, nickname,premium,active, email, image, genderInt, gender, description, henryLevel, likeReceived, likeGiven, matches} = req.body
 
     userSchema
-        .updateOne({_id:id},{ $set: {name, age, birthday, nickname,premium,active, email, image, genderInt, gender, description, henryLevel, likeRecieved, likeGiven, matches}})
+        .updateOne({_id:id},{ $set: {name, age, birthday, nickname,premium,active, email, image, genderInt, gender, description, henryLevel, likeReceived, likeGiven, matches}})
+
         .then(data => res.json(data))
         .catch((error) => res.json({message:error}));
 }
 
 module.exports = {
-  userPut,
+    userPut
 };
