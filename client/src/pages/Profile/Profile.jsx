2b660a40ed92f31195b3c5271dd27ee1fdf91ca0
zayrarepositor <zayra.contacto@gmail.com>
2022-07-04 14:17:28 -0300

    debugg

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -28,7 +28,7 @@ const Profile = () => {
 
   useEffect(() => {
     dispatch(getUserByNick(userProfile.nickname));
-    setUpdate(false)
+    setUpdate(false);
   }, [update]);
 
   return (
@@ -37,8 +37,8 @@ const Profile = () => {
       {isAuthenticated ? (
         <div className="todojunto">
           <div className="informaciondelusuario">
-            <Link to='/'>
-              <button className="returnHomeButton" >Return to Home</button>
+            <Link to="/">
+              <button className="returnHomeButton">Return to Home</button>
             </Link>
             <Avatar
               src={user.picture}
@@ -49,19 +49,68 @@ const Profile = () => {
             <Typography variant="h2" color="text.secondary">
               {user.name}
             </Typography>
-           {/*  <h1 className="perfil">Perfil del Usuario</h1> */}
-            <div className="label">{userProfile.name ? <p> El nombre que elegiste para mostrar: {userProfile.name} </p> : <p> Todavia no ingresaste un nombre para mostrar</p>}</div>
+            {/*  <h1 className="perfil">Perfil del Usuario</h1> */}
+            <div className="label">
+              {userProfile.name ? (
+                <p> El nombre que elegiste para mostrar: {userProfile.name} </p>
+              ) : (
+                <p> Todavia no ingresaste un nombre para mostrar</p>
+              )}
+            </div>
 
-            <div className="label">{userProfile.image.length >= 0 ? <img src={userProfile.image[0]} alt={userProfile.name} className="imagenperfil" /> : <p className="alert"> Debes cargar una imagen </p>}</div>
-            <div className="label">{userProfile.age ? <p> La edad que declaraste es: {userProfile.age} </p> : <p className="alert"> Todavia no ingresaste tu edad</p>}</div>
-            <div className="label">{userProfile.email ? <p> Tu email: {userProfile.email} </p> : <p className="alert"> Todavia no ingresaste tu email</p>}</div>
-            <div className="label">{userProfile.gender ? <p> Te definiste como {userProfile.gender} </p> : <p className="alert"> Todavia no definiste tu género</p>}</div>
-            <div className="label">{userProfile.genderInt ? <p> Te interesa conectar con {userProfile.genderInt} </p> : <p className="alert"> Todavia no definiste tu interes </p>} </div>
-            <div className="label">{userProfile.description ? <p> Tu Descripcion: {userProfile.description} </p> : <p className="alert"> Ingresa una breve descripcion tuya</p>}</div>
+            <div className="label">
+              {userProfile.image.length >= 0 ? (
+                <img
+                  src={userProfile.image[0]}
+                  alt={userProfile.name}
+                  className="imagenperfil"
+                />
+              ) : (
+                <p className="alert"> Debes cargar una imagen </p>
+              )}
+            </div>
+            <div className="label">
+              {userProfile.age ? (
+                <p> La edad que declaraste es: {userProfile.age} </p>
+              ) : (
+                <p className="alert"> Todavia no ingresaste tu edad</p>
+              )}
+            </div>
+            <div className="label">
+              {userProfile.email ? (
+                <p> Tu email: {userProfile.email} </p>
+              ) : (
+                <p className="alert"> Todavia no ingresaste tu email</p>
+              )}
+            </div>
+            <div className="label">
+              {userProfile.gender ? (
+                <p> Te definiste como {userProfile.gender} </p>
+              ) : (
+                <p className="alert"> Todavia no definiste tu género</p>
+              )}
+            </div>
+            <div className="label">
+              {userProfile.genderInt ? (
+                <p> Te interesa conectar con {userProfile.genderInt} </p>
+              ) : (
+                <p className="alert"> Todavia no definiste tu interes </p>
+              )}{" "}
+            </div>
+            <div className="label">
+              {userProfile.description ? (
+                <p> Tu Descripcion: {userProfile.description} </p>
+              ) : (
+                <p className="alert"> Ingresa una breve descripcion tuya</p>
+              )}
+            </div>
 
             <h3> Tus Imagenes cargadas </h3>
             <br />
-            <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+            <ImageList
+              sx={{ width: 500, height: 450 }}
+              cols={3}
+              rowHeight={164}>
               {userProfile.image.map((item) => (
                 <ImageListItem key={userProfile.image}>
                   <img
