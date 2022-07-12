65b8b65379298deb4056a7842b92a8a579c33095
Rominavz <vz_romina@hotmail.com>
2022-07-11 21:13:44 -0300

    jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { ThemeProvider } from "@mui/system";
 //import reportWebVitals from "./reportWebVitals";?????(ZAYRA)
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store } from "./Redux/store";
 /* PARA LOS COLORES => import { green, purple } from "@mui/material/colors"; (ZAYRA)*/
 import { theme } from "./theme";
 import { App } from "./App";
