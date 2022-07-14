3ea4ba9213607301afbe8c754ad94de443174553
Rominavz <vz_romina@hotmail.com>
2022-07-13 23:51:46 -0300

    correcciones

--- client/src/pages/Landing/Slider.jsx
+++ client/src/pages/Landing/Slider.jsx
@@ -4,10 +4,6 @@ import styles from "./Slider.module.css";
 import img0 from "../../assets/img0.jpg";
 import img20 from "../../assets/img20.jpg";
 import img3 from "../../assets/img3.jpg";
-<<<<<<< HEAD:client/src/components/LandingPage/Slider.jsx
-
-=======
->>>>>>> 1356c82492c0be506c6529ace2eb267741467f04:client/src/pages/Landing/Slider.jsx
 
 function Slider() {
   return (
@@ -16,13 +12,7 @@ function Slider() {
         <img src={img0} alt="slide 1" />
         <img src={img20} alt="slide 2" />
         <img src={img3} alt="slide 3" />
-<<<<<<< HEAD:client/src/components/LandingPage/Slider.jsx
-         
-    </figure>  
-   
-=======
       </figure>
->>>>>>> 1356c82492c0be506c6529ace2eb267741467f04:client/src/pages/Landing/Slider.jsx
     </div>
   );
 }
